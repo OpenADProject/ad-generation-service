0296babcc6d5d4760ce875c1ad830c03db2e5c59
ysh <yoonsnowman@gmail.com>
2025-09-19 01:36:53 +0000

    feat: add user_models API in backend

--- src/backend/models.py
+++ src/backend/models.py
@@ -2,32 +2,62 @@ from sqlmodel import SQLModel, Field, Column, Text
 from typing import Optional
 from datetime import datetime
 
-# 생성/조회/응답에 공통으로 사용할 모델
+
+##################################################
+# 사용자 콘텐츠 생성 결과물 관리
+##################################################
+# 공통 모델
 class GenerationBase(SQLModel):
     input_image_path: Optional[str] = Field(default=None)
     input_text: Optional[str] = Field(default=None)
     output_text: Optional[str] = Field(default=None, sa_column=Column(Text))
     output_image_path: Optional[str] = Field(default=None)
 
-# DB 테이블 구조 정의
+# DB 정의
 class Generation(GenerationBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     created_at: datetime = Field(default_factory=datetime.utcnow, nullable=False)
 
-# 생성 이력 수신
+# 수신
 class GenerationCreate(GenerationBase):
     pass
 
-# 생성 이력 송신
+# 송신
 class GenerationResponse(GenerationBase):
     id: int
     created_at: datetime
 
-# 성공/실패 응답 모델
+# 응답
 class Message(SQLModel):
     ok: bool
     message: str
 
+
+##################################################
+# 사용자 인물&동물 모델 관리
+##################################################
+# 공통 모델
+class UserModelBase(SQLModel):
+    # 파일 경로 저장할 필드
+    file_path: str
+    alias: str
+
+# DB 정의
+class UserModel(UserModelBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    is_deletable: bool = Field(default=True) # 삭제 가능 여부(사용자에게 미노출)
+
+# 수신
+class UserModelCreate(UserModelBase):
+    pass
+
+# 수정
+class UserModelUpdate(SQLModel):
+    alias: str # 별명만 받음
+
+# 송신
+class UserModelResponse(UserModelBase):
+    id: int
 
 
 
